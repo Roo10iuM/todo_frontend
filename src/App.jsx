30aa35649542b66edf7c9daa0201bc28cc7bd5d8
Roo10iuM <danil5400000@gmail.com>
2025-12-16 19:35:15 +0300

    Add dashboard and pages draft

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import Home from './pages/home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import styles from './App.module.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const Pages = {
+    "HOME": "home",
+    "SIGNIN": "signin",
+    "SIGNUP": "signup",
+  }
+
+  const [page, setPage] = useState(Pages.HOME);
+
+  function pageController(page) {
+    switch (page) {
+      case Pages.HOME:
+        return <Home />;
+      case Pages.SIGNIN:
+        return <SignIn />;
+      case Pages.SIGNUP:
+        return <SignUp />;
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className={styles.dashboard}>
+        <button onClick={() => setPage(Pages.HOME)}>Home</button>
+        <div>
+          <button onClick={() => setPage(Pages.SIGNIN)}>Sign in</button>
+          <button onClick={() => setPage(Pages.SIGNUP)}>Sign up</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className={styles.outlet}>
+        {pageController(page)}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
